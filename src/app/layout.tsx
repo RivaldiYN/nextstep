8405a8431af0bca0906043167c6326c814ecf242
RivaldiYN <88421969+RivaldiYN@users.noreply.github.com>
2025-04-30 14:56:39 +0700

    Add new favicon and apple touch icon images
    - Added apple-touch-icon.png with a size of 180x180 pixels.
    - Added favicon-16x16.png for browser tab icon.
    - Added favicon-32x32.png for high-resolution browser tab icon.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -61,8 +61,8 @@ export const metadata: Metadata = {
     nocache: false
   },
   icons: {
-    icon: "/favicon.ico",
-    shortcut: "/favicon-16x16.png",
+    icon: "/favicon-16x16.png",
+    shortcut: "/favicon-32x32.png",
     apple: "/apple-touch-icon.png"
   },
   manifest: "/site.webmanifest"
